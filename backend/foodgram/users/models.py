bd41917688d2cc7f3b59cd7e8f24a042b75e837a
Анастасия Белобородова <beloborodova.anastasiia@yandex.ru>
2022-10-04 15:21:54 +0500

    Added favorite, shopping in admin

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import TokenProxy
 
 
 class User(AbstractUser):
@@ -39,6 +39,7 @@ class Subscribe(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор'
     )
 
     class Meta:
@@ -52,7 +53,7 @@ class Subscribe(models.Model):
         verbose_name = 'Подписки'
 
 
-class CustomToken(Token):
+class CustomToken(TokenProxy):
     class Meta:
         verbose_name_plural = 'Токены'
         verbose_name = 'Токен'
