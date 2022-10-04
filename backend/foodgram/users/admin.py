7f37fff9996fee59b680a2ed67d91f1f949feb57
Анастасия Белобородова <beloborodova.anastasiia@yandex.ru>
2022-10-04 16:17:40 +0500

    Russification token admin

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from rest_framework.authtoken.admin import TokenAdmin
 
 from recipes.models import Favorite, Shopping
 
-from .models import CustomToken, Subscribe, User
+from .models import Subscribe, User
 
 
 class SubscribeInline(admin.TabularInline):
